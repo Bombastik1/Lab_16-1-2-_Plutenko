b020b637ba332aecf90a97efc9407b3ded2741d7
Bombastik1 <plutenko.ay@gmail.com>
2024-01-24 21:21:17 +0200

    Добавьте файлы проекта.

---
+++ Lab_16(1-2)_Plutenko/Lab_16(1-2)_Plutenko.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale> 
+
+int main() {
+    setlocale(LC_ALL, "");
+    int i, j;
+    int a[4][4] = { 11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43, 44 };
+
+    printf("Матриця:\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%2d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    int s = 0;
+    int d = 1;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < i; j++) {
+            s += a[i][j];
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int m = a[i][0];
+        for (int j = 0; j < 4; j++) {
+            if (a[i][j] > m) {
+                m = a[i][j];
+            }
+        }
+        d *= m;
+    }
+
+    printf("Сума елементiв вище головної дiагоналi: %d\n", s);
+    printf("\nДобуток максимальних елементiв кожного рядка: %d\n", d);
+
+    return 0;
+}
